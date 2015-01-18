b08888ef6f4f04005ef41b6ebecd153fcd55e8fa
andychims <andychims@gmail.com>
2015-01-17 23:44:20 -0500

    updates

--- app.js
+++ app.js
@@ -2,70 +2,6 @@
 $(document).ready(function(){
 
 
-//
-//  PIANO ROLL 
-//
-
-   // window.AudioContext = window.AudioContext || window.webkitAudioContext;
-
-   var context = new AudioContext(),
-       settings = {
-			  id: 'keyboard',
-	        width: 900,
-	        height: 150,
-	        octaves: 2,
-	        startNote: 'A3',
-	        whiteNotesColour: 'white',
-	        blackNotesColour: 'black',
-	        hoverColour: '#f3e939'
-       },
-       keyboard = new QwertyHancock(settings),
-       freqMod = 0;
-
-
-
-   masterGain = context.createGain();
-   nodes = [];
-   masterGain.gain.value = 0.1;
-   masterGain.connect(context.destination); 
-   
-
-   osc2Gain = context.createGain();
-   osc2Gain.gain.value = 0.9;
-   osc2Gain.connect(context.destination); 
-
-   keyboard.keyDown = function (note, frequency) {
-       var oscillator = context.createOscillator();
-       oscillator.type = 'square';
-       oscillator.frequency.value = frequency + freqMod;
-       oscillator.connect(masterGain);
-       oscillator.start(0);
-       nodes.push(oscillator);
-
-       // var oscillator2 = context.createOscillator();
-       // oscillator2.type = 'saw';
-       // oscillator2.frequency.value = frequency;
-       // oscillator2.connect(osc2Gain);
-       // oscillator2.start(0);
-       // nodes.push(oscillator2);
-   };
-
-   keyboard.keyUp = function (note, frequency) {
-       var new_nodes = [];
-
-       for (var i = 0; i < nodes.length; i++) {
-           if (Math.round(nodes[i].frequency.value) === Math.round(frequency + freqMod)) {
-               nodes[i].stop(0);
-               nodes[i].disconnect();
-           } else {
-               new_nodes.push(nodes[i]);
-           }
-       }
-
-       nodes = new_nodes;
-   };
-
-
 
 
 
@@ -307,6 +243,13 @@ $(document).ready(function(){
 	}
 
 
+
+
+
+
+
+
+
 // 
 // 
 // 
@@ -342,6 +285,71 @@ $(document).ready(function(){
 
 
 
+
+
+
+//
+//  PIANO ROLL 
+//
+
+   // window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+   var context = new AudioContext(),
+       settings = {
+			  id: 'keyboard',
+	        width: 900,
+	        height: 150,
+	        octaves: 2,
+	        startNote: 'A3',
+	        whiteNotesColour: 'white',
+	        blackNotesColour: 'black',
+	        hoverColour: '#f3e939'
+       },
+       keyboard = new QwertyHancock(settings),
+       freqMod = 0;
+
+
+
+   masterGain = context.createGain();
+   nodes = [];
+   masterGain.gain.value = 0.1;
+   masterGain.connect(context.destination); 
+   
+
+   osc2Gain = context.createGain();
+   osc2Gain.gain.value = 0.9;
+   osc2Gain.connect(context.destination); 
+
+   keyboard.keyDown = function (note, frequency) {
+       var oscillator = context.createOscillator();
+       oscillator.type = 'square';
+       oscillator.frequency.value = frequency + freqMod;
+       oscillator.connect(masterGain);
+       oscillator.start(0);
+       nodes.push(oscillator);
+
+       // var oscillator2 = context.createOscillator();
+       // oscillator2.type = 'saw';
+       // oscillator2.frequency.value = frequency;
+       // oscillator2.connect(osc2Gain);
+       // oscillator2.start(0);
+       // nodes.push(oscillator2);
+   };
+
+   keyboard.keyUp = function (note, frequency) {
+       var new_nodes = [];
+
+       for (var i = 0; i < nodes.length; i++) {
+           if (Math.round(nodes[i].frequency.value) === Math.round(frequency + freqMod)) {
+               nodes[i].stop(0);
+               nodes[i].disconnect();
+           } else {
+               new_nodes.push(nodes[i]);
+           }
+       }
+
+       nodes = new_nodes;
+   };
 
 
 
